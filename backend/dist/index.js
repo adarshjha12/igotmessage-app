b9b83747a9c8489244334cfc7c02a09f0d962431
adarsh <jhaa50872@gmail.com>
2025-02-25 15:47:14 +0530

    initialize backend with TypeScript setup, .gitignore, and basic hello world function

---
+++ backend/dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+function hello(arg) {
+    return `hii ${arg}`;
+}
+hello('ts');
---
+++ backend/src/index.ts
@@ -0,0 +1,6 @@
+function hello(arg: string) : string {
+    return `hii ${arg}`
+
+}
+
+console.log(hello('ts'));
