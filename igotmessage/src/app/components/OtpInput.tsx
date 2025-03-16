693ccbc85171c2348dcb1ff0fc44dfefb55b6d61
adarsh <jhaa50872@gmail.com>
2025-03-17 01:25:43 +0530

    Implement OtpInput component with focus management for input fields

--- igotmessage/src/app/components/OtpInput.tsx
+++ igotmessage/src/app/components/OtpInput.tsx
@@ -2,10 +2,30 @@ import React, {useRef} from 'react'
 
 function OtpInput() {
   
-  // const move
+  const inputsRef = useRef<Array<HTMLInputElement | null>>([])
+
+  const moveNext = function (current: HTMLInputElement, index: number ) {
+    if (current.value.length === 1 && index < inputsRef.current.length - 1) {
+      inputsRef.current[index + 1]?.focus()
+    }
+  }
+
   return (
-    <div>
-      
+    <div className='flex gap-3'>
+      {
+        Array.from({length: 4}, (_, i) =>{
+          return (
+            <input 
+            key={i}
+            type="text"
+            ref={(el) => {inputsRef.current[i] = el}}
+            onInput={(e) => moveNext(e.target as HTMLInputElement, i)}
+            maxLength={1}
+            className='bg-amber-400 text-black rounded-sm w-[20px]' 
+            />
+          )
+        })
+      }
     </div>
   )
 }
