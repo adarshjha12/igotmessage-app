60eead511239d2b7ab19eb9a166e59338ca0c7dc
adarsh <jhaa50872@gmail.com>
2025-03-17 20:13:54 +0530

    Refactor OtpInput component to streamline input handling and improve code readability

--- igotmessage/src/app/components/OtpInput.tsx
+++ igotmessage/src/app/components/OtpInput.tsx
@@ -1,45 +1,44 @@
-import React, {useRef} from 'react'
+import React, {useRef, useEffect} from 'react'
 
 function OtpInput() {
-  
+
   const inputRefs = useRef<Array<HTMLInputElement | null>>([])
 
-  const moveNext = function (userInput: HTMLInputElement, index: number ) {
+  const moveNext = function (userInput: HTMLInputElement, index: number) {
     if (userInput.value.length === 1 && index < inputRefs.current.length - 1) {
       inputRefs.current[index + 1]?.focus()
     }
   }
-
-  const moveBack = (userInput: HTMLInputElement, index: number) => {
+  const moveBack = function (userInput: HTMLInputElement, index: number) {
     if (userInput.value.length === 0 && index > 0) {
-      inputRefs.current[index - 1]?.focus();
+      inputRefs.current[index - 1]?.focus()
     }
-  };
-  
+  }
 
   return (
-    <div className='flex gap-3'>
-      {
-        Array.from({length: 4}, (_, i) =>{
+    <div className='flex gap-2.5'>
+        {Array.from({length: 4}, (_, i) => {
           return (
             <input 
-            key={i}
             type="text"
-            ref={(el) => {
-              inputRefs.current[i] = el
+            maxLength={1}
+            key={i} 
+            ref={(element) => {
+              inputRefs.current[i] = element
             }}
-            onInput={(e) => moveNext(e.target as HTMLInputElement, i)}
+            className='w-[30px] pl-2 rounded-md  bg-blue-800 outline-none border-2 border-white  opacity-75'
+            onInput={(e) => {
+              moveNext(e.target as HTMLInputElement, i)
+            }}
+
             onKeyDown={(e) =>{
               if (e.key === 'Backspace') {
                 moveBack(e.target as HTMLInputElement, i)
               }
             }}
-            maxLength={1}
-            className='bg-amber-400 pl-1 text-black rounded-sm w-[20px]' 
             />
           )
-        })
-      }
+        })}
     </div>
   )
 }
