65acfd1c39a2079c69a0c8df4630b9699e39ee33
adarsh <jhaa50872@gmail.com>
2025-04-25 23:54:42 +0530

    Enhance getCurrentUser route: add token validation and improve error handling

--- backend/src/routers/currentUser.ts
+++ backend/src/routers/currentUser.ts
@@ -3,12 +3,22 @@ import jwt from 'jsonwebtoken'
 
 const getCurrentUser = Router()
 
+interface userPayload {
+    id?: string
+    email?: string
+}
+
 getCurrentUser.get('/get-current-user', async (req, res) =>{
     const token = req.cookies.authToken
+    if (!token) {
+        res.status(401).json({success: false, message: 'no token provided'})
+        return
+    }
 
     try {
-        
+        const decoded = jwt.verify(token, process.env.JWT_SECRET!) as userPayload
     } catch (error) {
+        console.log(error);
         
     }
 })
