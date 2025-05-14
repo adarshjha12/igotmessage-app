a63f56d58b2fee9ef7237793ce5904d2deb86d29
adarsh <jhaa50872@gmail.com>
2025-05-14 20:17:02 +0530

    Refactor ESLint configuration: migrate from .mjs to .ts format and add TypeScript parser for improved linting support

---
+++ igotmessage/eslint.config.ts
@@ -0,0 +1,31 @@
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat: any = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+export default [
+  ...compat.extend([
+    "next/core-web-vitals",       
+    "next",                     
+    "plugin:@typescript-eslint/recommended", 
+  ]),
+
+  {
+    rules: {
+      "no-unused-vars": "warn",                         
+      "no-console": "off",                              
+      "react/prop-types": "off",                         
+      "@typescript-eslint/no-unused-vars": "warn",        
+      "@typescript-eslint/no-explicit-any": "warn",      
+      "react/jsx-max-props-per-line": ["warn", {  
+      "maximum": 3
+      }],
+    },
+  },
+];
