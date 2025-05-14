04b2b9a25485a50f3255043df6f4584a38c786b1
adarsh <jhaa50872@gmail.com>
2025-05-14 20:25:11 +0530

    Refactor ESLint configuration: switch from compat.config to compat.extend for improved compatibility

--- igotmessage/eslint.config.ts
+++ igotmessage/eslint.config.ts
@@ -10,23 +10,21 @@ const compat: any = new FlatCompat({
 });
 
 export default [
-  ...compat.config({
-    extends: [
-      "next/core-web-vitals",       
-      "next",
-      "plugin:@typescript-eslint/recommended",
-    ],
-  }),
+  ...compat.extend([
+    "next/core-web-vitals",       
+    "next",                       
+    "plugin:@typescript-eslint/recommended", 
+  ]),
 
   {
     rules: {
-      "no-unused-vars": "warn",                         
-      "no-console": "off",                              
+      "no-unused-vars": "warn",                          
+      "no-console": "off",                               
       "react/prop-types": "off",                         
       "@typescript-eslint/no-unused-vars": "warn",        
-      "@typescript-eslint/no-explicit-any": "warn",      
-      "react/jsx-max-props-per-line": ["warn", {  
-      "maximum": 3
+      "@typescript-eslint/no-explicit-any": "warn",
+      "react/jsx-max-props-per-line": ["warn", {         
+        "maximum": 3
       }],
     },
   },
