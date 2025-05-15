271ccf632553a4299803a09f9c68c95e1d15cc58
adarsh <jhaa50872@gmail.com>
2025-05-15 19:24:12 +0530

    Fix file path in getOtpEmailHtml function for correct template loading

--- backend/src/utils/loadTemplate.ts
+++ backend/src/utils/loadTemplate.ts
@@ -3,7 +3,7 @@ import fs from 'fs';
 import path from 'path';
 
 export function getOtpEmailHtml({otp} : {otp: string}) {
-  const filePath = path.join(process.cwd(), '/src/templates', 'otpTemplate.html');
+  const filePath = path.join(process.cwd(), 'src', 'templates', 'otpTemplate.html');
   let template = fs.readFileSync(filePath, 'utf-8');
   return template.replace('{{otp}}', otp);
 }
