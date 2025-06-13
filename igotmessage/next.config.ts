59cecc13f95e9f73ed1800c0dd46ed915dfbe29a
adarsh <jhaa50872@gmail.com>
2025-06-13 17:25:48 +0530

    Refactor next.config.ts to simplify configuration and remove PWA integration

--- igotmessage/next.config.ts
+++ igotmessage/next.config.ts
@@ -1,64 +1,7 @@
-import withPWA from 'next-pwa';
+import { NextConfig } from 'next'
 
-const baseConfig = {
-  reactStrictMode: true,
- };
+const nextConfig: NextConfig = {
+    reactStrictMode: true,
+}
 
-const nextConfig = withPWA({
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
-  register: true,
-  skipWaiting: true,
-  buildExcludes: [/app-build-manifest\.json$/],
-  runtimeCaching: [
-    {
-      // Static assets
-      urlPattern: /\.(?:js|css|woff2?|png|jpg|jpeg|svg|gif)$/i,
-      handler: 'StaleWhileRevalidate',
-      options: {
-        cacheName: 'static-assets',
-        cacheableResponse: {
-          statuses: [0, 200],
-        },
-        expiration: { maxEntries: 100, maxAgeSeconds: 60 * 60 * 24 * 30 },
-      },
-    },
-    {
-      urlPattern: /^\/dash\/.*$/i,
-      handler: 'StaleWhileRevalidate', // or 'NetworkFirst' if dynamic
-      options: {
-        cacheName: 'dashboard-pages',
-        expiration: { maxEntries: 20, maxAgeSeconds: 60 * 60 * 12},
-        cacheableResponse: { statuses: [0, 200] },
-      },
-    },
-    {
-      // HTML pages (routes)
-      urlPattern: /^\/.*$/i,
-      handler: 'StaleWhileRevalidate',
-      options: {
-        cacheName: 'page-cache',
-        cacheableResponse: {
-          statuses: [0, 200],
-        },
-        expiration: { maxEntries: 50, maxAgeSeconds: 60 * 60 * 24 },
-      },
-    },
-    {
-      // API
-      urlPattern: /^https:\/\/igotmessage-app-backend\.onrender\.com\/api\/.*$/i,
-      handler: 'NetworkFirst',
-      options: {
-        cacheName: 'api-cache',
-        cacheableResponse: {
-          statuses: [0, 200],
-        },
-        networkTimeoutSeconds: 10,
-        expiration: { maxEntries: 30, maxAgeSeconds: 60 * 5 },
-      },
-    },
-  ]
-  
-})(baseConfig);
-
-export default nextConfig;
+export default nextConfig
