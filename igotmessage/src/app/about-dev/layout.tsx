b323b578775d938ced4f9cca68191734f075028c
adarsh <jhaa50872@gmail.com>
2025-06-15 20:13:23 +0530

    Remove unused loading components and simplify layout structure

--- igotmessage/src/app/about-dev/layout.tsx
+++ igotmessage/src/app/about-dev/layout.tsx
@@ -1,12 +1,9 @@
 import Loader from '@/components/Loader'
 import React, {ReactNode} from 'react'
-import { Suspense } from 'react'
 
 function layout({children} : {children: ReactNode}) {
   return (
-    <Suspense fallback={<Loader animate={true} scaleMd={true} color='black' />} >
-        {children}
-    </Suspense>
+    children
   )
 }
 
--- igotmessage/src/app/dash/notifications/loading.tsx
+++
@@ -1,9 +0,0 @@
-// app/dash/feed/loading.tsx
-
-export default function FeedLoading() {
-  return (
-    <div className="h-screen w-full flex items-center justify-center bg-red-500  text-[var(--bgColor)]">
-      <div className="animate-spin rounded-full h-12 w-12 border-4 border-t-transparent border-[var(--bgColor)]" />
-    </div>
-  )
-}
