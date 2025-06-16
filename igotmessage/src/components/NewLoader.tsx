6770322d01f322e89ecbd8298cc2dd6a643b45cf
adarsh <jhaa50872@gmail.com>
2025-06-16 15:59:03 +0530

    fix: update loader border color and remove unused selector in SplashScreen; remove Dashboard component

--- igotmessage/src/components/NewLoader.tsx
+++ igotmessage/src/components/NewLoader.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function NewLoader() {
   return (
     <div className="flexitems-center justify-center">
-      <div className="w-8 h-8 border-4 border-[var(--borderColor)] rotate-loader border-r-gray-700 rounded-full "></div>
+      <div className="w-8 h-8 border-4 border-[var(--borderColor)] rotate-loader border-r-transparent rounded-full "></div>
     </div>
   
   )
