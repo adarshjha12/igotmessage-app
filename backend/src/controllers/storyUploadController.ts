87bc999abfb8ca22ba74e13968cf1a9fe0b8316d
adarsh <jhaa50872@gmail.com>
2025-07-03 18:18:14 +0530

    refactor: add type annotations to storyUploadController for improved type safety

--- backend/src/controllers/storyUploadController.ts
+++ backend/src/controllers/storyUploadController.ts
@@ -1,5 +1,9 @@
-const storUploadController = async (req, res) => {
+import { Request, Response } from "express";
+
+const storUploadController = async (req: Request, res: Response) => {
     res.status(200).json({ message: "success" });
+    console.log(req);
+    
 };
 
 export default storUploadController;
