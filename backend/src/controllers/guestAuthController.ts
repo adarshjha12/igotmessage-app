ca151a19698f6d95037e25f7891273d8dbfb9949
adarsh <jhaa50872@gmail.com>
2025-08-21 20:04:36 +0530

    fix: Refactor handleGuestAuth function for improved formatting and consistency

--- backend/src/controllers/guestAuthController.ts
+++ backend/src/controllers/guestAuthController.ts
@@ -1,11 +1,10 @@
 import { Request, Response } from "express";
 import { User } from "../models/userModel";
 import jwt from "jsonwebtoken";
-import { log } from "console";
 
 const randomNumber = Math.floor(Math.random() * 100000 + 1);
 const redirectUrl =
-  process.env.NODE_ENV === 'production'
+  process.env.NODE_ENV === "production"
     ? `${process.env.PROD_FRONTEND_URL}/dash/feed`
     : `${process.env.DEV_FRONTEND_URL}/dash/feed`;
 
@@ -15,8 +14,8 @@ async function handleGuestAuth(req: Request, res: Response): Promise<any> {
       userName: "guest" + randomNumber,
       isGuest: true,
     });
-    user.save()
-    const payload = {id: user._id.toString(), username: user.userName}
+    user.save();
+    const payload = { id: user._id.toString(), username: user.userName };
     const token = jwt.sign(payload, process.env.JWT_SECRET!);
 
     res.cookie("authToken", token, {
@@ -26,12 +25,16 @@ async function handleGuestAuth(req: Request, res: Response): Promise<any> {
       maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
     });
 
-    res.status(201).json({success: true, message: `guest user guest${randomNumber} created successfully.`})
-
+    return res.status(201).json({
+      success: true,
+      message: `guest user guest${randomNumber} created successfully.`,
+    });
   } catch (error) {
     console.log(error);
-    
-     res.status(500).json({success: false, message: `IGotMessage server error`, error})
+
+    return res
+      .status(500)
+      .json({ success: false, message: `IGotMessage server error`, error });
   }
 }
 
